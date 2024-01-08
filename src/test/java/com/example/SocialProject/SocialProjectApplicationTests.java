04c7ee689758ae671768a3c1b400b8109ad5b228
Kristjan <kristjan.tepper@gmail.com>
2024-01-08 12:42:15 +0200

    Uploading files

---
+++ src/test/java/com/example/SocialProject/SocialProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SocialProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
